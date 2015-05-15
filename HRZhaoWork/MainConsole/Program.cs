942f4be123f59eee2abc38d5a36fe4c4980dc8d3
hrzhao <rong17173@139.com>
2015-05-15 14:58:43 +0800

    log4net常用配置

--- HRZhaoWork/MainConsole/Program.cs
+++ HRZhaoWork/MainConsole/Program.cs
@@ -2,13 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MainConsole.com;
+using log4net.Config;
 
 namespace MainConsole
 {
     class Program
     {
+        private static log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
         static void Main(string[] args)
         {
+            //log4net.ILog logger = 
+            //XmlConfigurator.Configure(new System.IO.FileInfo(args[0]));
+            log.Info("Program Start!1");
+            log.Debug("Program Start!2");
+            log.Error("Program Start!3");
+
+            new Bar().DoIt();
+
+            //Console.WriteLine("helloWorld");
+            ConsoleKeyInfo keyInfo;
+            while(true)
+            {
+                keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.Q)
+                {
+                    log.Info("q=>break");
+                    break;
+                }
+            }
         }
     }
 }
---
+++ HRZhaoWork/MainConsole/com/Bar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace MainConsole.com
+{
+    public class Bar
+    {
+        private static readonly ILog log = LogManager.GetLogger("testApp.Logging");
+
+        public void DoIt()
+        {
+            log.Debug("Did it again!");
+        }
+
+    }
+}
